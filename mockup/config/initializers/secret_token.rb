130155e62f6b26fba24091b7605d3e563628f5b2
baronen <baronen@baronen.com>
2013-10-26 15:51:39 +0200

    rails new, it begins!

---
+++ mockup/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mockup::Application.config.secret_key_base = 'e5bf1e684fa73839e077e266769396f6d013084dfde80e0197f4ac30005dde6c2e7a25ab8cfea199fdb5e44e153c7e42dfd63291a274c1a8aa6ca4a289ddda67'
