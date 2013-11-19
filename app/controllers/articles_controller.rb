2a234ac40725232c8a0c47e43182b93c3f5e581e
alxl <alx.l@gmx.com>
2013-11-19 19:28:37 +0100

    Related content visas när man kollar en artikel, 4st diy/research och en random relaterad commercial.
    Signed-off-by: alxl <alx.l@gmx.com>

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,6 +10,8 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params.require(:id))
+    @related = Article.where("type != 'Commercial' AND method = ? AND produce = ? AND id != ?", @article.method, @article.produce, @article.id).order("updated_at DESC").limit(4)
+    @rel_com = Article.where("type = 'Commercial' AND method = ? AND produce = ? AND id != ?", @article.method, @article.produce, @article.id).offset(rand(Article.count)).first
   end
 
   def new
