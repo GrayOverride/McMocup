f9ff568c4247d281b5bce0c84edb733762fa58f3
baronen <baronen@baronen.com>
2013-11-23 13:00:44 +0100

    branch

--- app/assets/javascripts/wizard.js
+++ app/assets/javascripts/wizard.js
@@ -1,55 +1,21 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
-jQuery(function($) {
-				// Example 1: Basic wizard with validation
-				$("#example-1").wizard({
-					stepsWrapper: "#wrapped",
-					submit: ".submit",
-					beforeSelect: function( event, state ) {
-						var inputs = $(this).wizard('state').step.find(':input');
-						return !inputs.length || !!inputs.valid();
-					}
-				}).wizard('form').submit(function( event ) {
-					event.preventDefault();
-					alert('Form submitted!');
+$(document).ready(function(){
+// Basic branching wizard
+	$("#brancher").wizard({
+	transitions: {
+		color: function( $step, action ) {
+			var branch = $step.find("[name=color]:checked").val();
 
-				}).validate({
-					errorPlacement: function(error, element) { 
-						if ( element.is(':radio') || element.is(':checkbox') ) {
-							error.insertBefore( element.next() );
+			if (!branch) {
+				alert("Please select a value to continue.");
+			}
 
-						} else { 
-							error.insertAfter( element );
-						}
-					}
-				});
+			return branch;
+		}
+	}
+	});
 
-				// Example 2: Basic wizard with progress bar
-				$("#progressbar").progressbar();
-
-				$("#example-2").wizard({
-					afterSelect: function( event, state ) {
-						$("#progressbar").progressbar("value", state.percentComplete);
-						$("#location").text("(" + state.stepsComplete + "/" + state.stepsPossible + ")");
-					}
-				});
-
-				// Example 3: Basic branching wizard
-				$("#example-3").wizard({
-					transitions: {
-						color: function( $step, action ) {
-							var branch = $step.find("[name=color]:checked").val();
-
-							if (!branch) {
-								alert("Please select a value to continue.");
-							}
-
-							return branch;
-						}
-					}
-				});
-
-				// Example 4: Nested Branches
-				$("#example-4").wizard();
-			});
+});
+function debug(){
+	console.log("sluta läsa konsolen");		
+}			
+debug();
