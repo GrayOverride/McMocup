0d64365c5e72666075024d5cf2db2f23df3c6ff0
Alexander Lindgren <alx.l@gmx.com>
2013-11-23 11:44:25 +0100

    Search and list articles, login error handling, turbolinks-fix and so on and so forth
    Signed-off-by: Alexander Lindgren <alx.l@gmx.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,15 +6,19 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	user = User.authenticate(params.require(:username), params.require(:password))
-  	if user
-  		session[:user_id] = user.id
+    if params[:username].present? && params["password"].present?
+  	 user = User.authenticate(params.require(:username), params.require(:password))
 
-  		redirect_to articles_path, :notice => "Logged in"
-  	else
+    	if user
+    		session[:user_id] = user.id
 
-  		redirect_to root_url, :notice => "Error"
-  	end
+    		redirect_to articles_path, :notice => "Logged in"
+    	else
+    		redirect_to log_in_path, :notice => "Invalid credentials"
+    	end
+    else
+      redirect_to log_in_path, :notice => "An error occured, please make sure your information is correct"
+    end
   end
 
   def destroy
